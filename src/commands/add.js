736769b1950468246e970bf09dfe7dd6982f01f7
Isaac <git@eartharoid.me>
2021-02-25 15:49:15 +0000

    Consistency

--- src/commands/add.js
+++ src/commands/add.js
@@ -15,7 +15,7 @@ module.exports = {
 	aliases: ['none'],
 	example: 'add @member to #ticket-23',
 	args: true,
-	async execute(client, message, args, log, {config, Ticket}) {
+	async execute(client, message, args, log, { config, Ticket }) {
 		const guild = client.guilds.cache.get(config.guild);
 
 		const notTicket = new MessageEmbed()
