ef375f96c168ed7255476512306dc967b287a2c3
Puneet Gopinath <baalkrshna@gmail.com>
2021-08-16 20:21:58 +0530

    fix(guild)

--- src/structures/guild.js
+++ src/structures/guild.js
@@ -4,11 +4,11 @@ Guild.prototype.deleteSettings = async () => {
     const row = await this.settings;
     return await row.destroy();
 };
-Guild.prototype.getSettings = () => {
+Guild.prototype.getSettings = async () => {
     const data = { id: this.id };
-    const [settings] = await this.client.db.models.Guild.findOrCreate({
+    const settings = await this.client.db.models.Guild.findOrCreate({
         defaults: data,
         where: data
-    });
+    }).then(res => res[0]);
     return settings;
 };
