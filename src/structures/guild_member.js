d76f4a6e5522fa968cfb19427b9568ff67f5c196
Puneet Gopinath <baalkrshna@gmail.com>
2021-08-16 19:32:12 +0530

    refactor(guild_member)

--- src/structures/guild_member.js
+++ src/structures/guild_member.js
@@ -1,14 +1,6 @@
-const { Structures } = require('discord.js');
+const { GuildMember } = require('discord.js');
 
-Structures.extend('GuildMember', GuildMember => class extends GuildMember {
-	constructor(client, data, guild) {
-		super(client, data, guild);
-	}
-
-	async isStaff() {
-		const guild_categories = await this.client.db.models.Category.findAll({ where: { guild: this.guild.id } });
-
-		return guild_categories.some(cat => cat.roles.some(r => this.roles.cache.has(r)));
-	}
-
-});
+GuildMember.prototype.isStaff = async () => {
+    const guild_categories = await this.client.db.models.Category.findAll({ where: { guild: this.guild.id } });
+    return guild_categories.some(cat => cat.roles.some(r => this.roles.cache.has(r)));
+};
