4f5c1058e6f3e2e10c81529a956873755e60ab79
Puneet Gopinath <baalkrshna@gmail.com>
2021-08-16 19:24:58 +0530

    refactor(topic): update

--- src/commands/topic.js
+++ src/commands/topic.js
@@ -36,13 +36,13 @@ module.exports = class TopicCommand extends Command {
 		const t_row = await this.client.db.models.Ticket.findOne({ where: { id: message.channel.id } });
 
 		if (!t_row) {
-			return await message.channel.send(
-				new MessageEmbed()
+			return await message.channel.send({
+				embeds: [new MessageEmbed()
 					.setColor(settings.error_colour)
 					.setTitle(i18n('commands.topic.response.not_a_ticket.title'))
 					.setDescription(i18n('commands.topic.response.not_a_ticket.description'))
-					.setFooter(settings.footer, message.guild.iconURL())
-			);
+					.setFooter(settings.footer, message.guild.iconURL())]
+			});
 		}
 
 		await t_row.update({ topic: this.client.cryptr.encrypt(args) });
@@ -56,23 +56,23 @@ module.exports = class TopicCommand extends Command {
 			.replace(/{+\s?(tag|ping|mention)?\s?}+/gi, member.user.toString());
 		const opening_message = await message.channel.messages.fetch(t_row.opening_message);
 
-		await opening_message.edit(
-			new MessageEmbed()
+		await opening_message.edit({
+			embeds: [new MessageEmbed()
 				.setColor(settings.colour)
 				.setAuthor(member.user.username, member.user.displayAvatarURL())
 				.setDescription(description)
 				.addField(i18n('ticket.opening_message.fields.topic'), args)
-				.setFooter(settings.footer, message.guild.iconURL())
-		);
+				.setFooter(settings.footer, message.guild.iconURL())]
+		});
 
-		await message.channel.send(
-			new MessageEmbed()
+		await message.channel.send({
+			embeds: [new MessageEmbed()
 				.setColor(settings.success_colour)
 				.setAuthor(message.author.username, message.author.displayAvatarURL())
 				.setTitle(i18n('commands.topic.response.changed.title'))
 				.setDescription(i18n('commands.topic.response.changed.description'))
-				.setFooter(settings.footer, message.guild.iconURL())
-		);
+				.setFooter(settings.footer, message.guild.iconURL())]
+		});
 
 		this.client.log.info(`${message.author.tag} changed the topic of ${message.channel.id}`);
 	}
