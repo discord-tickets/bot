66b46cf3e27428a7f253d1daee948af79a49fbe8
Isaac <git@eartharoid.me>
2022-05-07 18:46:19 +0100

    Add PATCH guild settings route

--- src/routes/api/admin/guilds/[guild]/index.js
+++ src/routes/api/admin/guilds/[guild]/index.js
@@ -5,6 +5,22 @@ module.exports.get = fastify => ({
 
 		const settings = await client.prisma.guild.findUnique({ where: { id: req.params.guild } }) ??
 			await client.prisma.guild.create({ data: { id: req.params.guild } });
+
+		res.send(settings);
+	},
+	onRequest: [fastify.authenticate, fastify.isAdmin],
+});
+
+module.exports.patch = fastify => ({
+	handler: async (req, res) => {
+		/** @type {import('../../../../../client')} */
+		const client = res.context.config.client;
+
+		const settings = await client.prisma.guild.update({
+			data: req.body,
+			where: { id: req.params.guild },
+		});
+
 		res.send(settings);
 	},
 	onRequest: [fastify.authenticate, fastify.isAdmin],
