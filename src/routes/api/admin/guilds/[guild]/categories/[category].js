c04787bb80da7e9131e0c1f68e83611d0783941a
Isaac <git@eartharoid.me>
2022-07-17 23:05:12 +0100

    don't create a new category again

--- src/routes/api/admin/guilds/[guild]/categories/[category].js
+++ src/routes/api/admin/guilds/[guild]/categories/[category].js
@@ -58,37 +58,10 @@ module.exports.patch = fastify => ({
 		/** @type {import('client')} */
 		const client = res.context.config.client;
 		const categoryId = Number(req.params.category);
-		const user = await client.users.fetch(req.user.payload.id);
 		const guild = client.guilds.cache.get(req.params.guild);
 		const data = req.body;
-		const allow = ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES'];
 		const original = req.params.category && await client.prisma.category.findUnique({ where: { id: categoryId } });
 		if (!original) return res.status(404);
-
-		if (!data.discordCategory) {
-			const channel = await guild.channels.create(data.name, {
-				permissionOverwrites: [
-					...[
-						{
-							deny: ['VIEW_CHANNEL'],
-							id: guild.roles.everyone,
-						},
-						{
-							allow: allow,
-							id: client.user.id,
-						},
-					],
-					...data.staffRoles.map(id => ({
-						allow: allow,
-						id,
-					})),
-				],
-				position: 1,
-				reason: `Tickets category created by ${user.tag}`,
-				type: 'GUILD_CATEGORY',
-			});
-			data.discordCategory = channel.id;
-		}
 
 		const category = await client.prisma.category.update({
 			data: {
