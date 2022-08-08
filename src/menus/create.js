80399cd2940caa03b6bd59bbc646a8724bf77991
Isaac <git@eartharoid.me>
2022-08-08 23:36:45 +0100

    Fix Discord's UI problem

--- src/menus/create.js
+++ src/menus/create.js
@@ -13,6 +13,7 @@ module.exports = class CreateMenu extends Menu {
 	 * @param {import("discord.js").SelectMenuInteraction} interaction
 	 */
 	async run(id, interaction) {
+		interaction.message.edit({ components: interaction.message.components }); // reset the select menu (minor client-side UI issue)
 		await this.client.tickets.create({
 			categoryId: interaction.values[0],
 			interaction,
