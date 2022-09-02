3b165bad137c05e1aad819aa8d138831c05416e1
Isaac <git@eartharoid.me>
2022-09-02 19:59:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -113,7 +113,8 @@ class Bot extends Client {
 				Intents.FLAGS.GUILDS,
 				Intents.FLAGS.GUILD_MEMBERS,
 				Intents.FLAGS.GUILD_MESSAGES,
-				Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+				Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+				Intents.FLAGS.MESSAGE_CONTENT,
 			],
 			partials: [
 				'CHANNEL',
