15318df9e4325c0f89257b44f29f37af499b013a
Isaac <git@eartharoid.me>
2022-10-28 17:32:28 +0100

    feat: `/add` and `/remove` commands

--- src/commands/slash/add.js
+++ src/commands/slash/add.js
@@ -1,5 +1,8 @@
 const { SlashCommand } = require('@eartharoid/dbf');
 const { ApplicationCommandOptionType } = require('discord.js');
+const ExtendedEmbedBuilder = require('../../lib/embed');
+const { isStaff } = require('../../lib/users');
+const { logTicketEvent } = require('../../lib/logging');
 
 module.exports = class AddSlashCommand extends SlashCommand {
 	constructor(client, options) {
@@ -32,5 +35,110 @@ module.exports = class AddSlashCommand extends SlashCommand {
 		});
 	}
 
-	async run(interaction) { }
+	/**
+	 * @param {import("discord.js").ChatInputCommandInteraction} interaction
+	 */
+	async run(interaction) {
+		/** @type {import("client")} */
+		const client = this.client;
+
+		await interaction.deferReply({ ephemeral: true });
+
+		const ticket = await client.prisma.ticket.findUnique({
+			include: { guild: true },
+			where: { id: interaction.options.getString('ticket', false) || interaction.channel.id },
+		});
+
+		if (!ticket) {
+			const settings = await client.prisma.guild.findUnique({ where: { id: interaction.guild.id } });
+			const getMessage = client.i18n.getLocale(settings.locale);
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: settings.footer,
+					})
+						.setColor(settings.errorColour)
+						.setTitle(getMessage('misc.invalid_ticket.title'))
+						.setDescription(getMessage('misc.invalid_ticket.description')),
+				],
+			});
+		}
+
+		const getMessage = client.i18n.getLocale(ticket.guild.locale);
+
+		if (
+			ticket.id !== interaction.channel.id &&
+			ticket.createdById !== interaction.member.id &&
+			!(await isStaff(interaction.guild, interaction.member.id))
+		) {
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: ticket.guild.footer,
+					})
+						.setColor(ticket.guild.errorColour)
+						.setTitle(getMessage('commands.slash.add.not_staff.title'))
+						.setDescription(getMessage('commands.slash.add.not_staff.description')),
+				],
+			});
+		}
+
+		/** @type {import("discord.js").TextChannel} */
+		const ticketChannel = await interaction.guild.channels.fetch(ticket.id);
+		const member = interaction.options.getMember('member', true);
+
+		await ticketChannel.permissionOverwrites.edit(
+			member,
+			{
+				AttachFiles: true,
+				EmbedLinks: true,
+				ReadMessageHistory: true,
+				SendMessages: true,
+				ViewChannel: true,
+			},
+			`${interaction.user.tag} added ${member.user.tag} to the ticket`,
+		);
+
+		await ticketChannel.send({
+			embeds: [
+				new ExtendedEmbedBuilder()
+					.setColor(ticket.guild.primaryColour)
+					.setDescription(getMessage('commands.slash.add.added', {
+						added: member.toString(),
+						by: interaction.member.toString(),
+					})),
+			],
+		});
+
+		await interaction.editReply({
+			embeds: [
+				new ExtendedEmbedBuilder({
+					iconURL: interaction.guild.iconURL(),
+					text: ticket.guild.footer,
+				})
+					.setColor(ticket.guild.successColour)
+					.setTitle(getMessage('commands.slash.add.success.title'))
+					.setDescription(getMessage('commands.slash.add.success.description', {
+						member: member.toString(),
+						ticket: ticketChannel.toString(),
+					})),
+			],
+		});
+
+		logTicketEvent(this.client, {
+			action: 'update',
+			diff: {
+				original: {},
+				updated: { [getMessage('log.ticket.added')]: member.user.tag },
+			},
+			target: {
+				id: ticket.id,
+				name: `<#${ticket.id}>`,
+			},
+			userId: interaction.user.id,
+		});
+
+	}
 };
--- src/commands/slash/remove.js
+++ src/commands/slash/remove.js
@@ -1,5 +1,8 @@
 const { SlashCommand } = require('@eartharoid/dbf');
 const { ApplicationCommandOptionType } = require('discord.js');
+const ExtendedEmbedBuilder = require('../../lib/embed');
+const { isStaff } = require('../../lib/users');
+const { logTicketEvent } = require('../../lib/logging');
 
 module.exports = class RemoveSlashCommand extends SlashCommand {
 	constructor(client, options) {
@@ -32,5 +35,109 @@ module.exports = class RemoveSlashCommand extends SlashCommand {
 		});
 	}
 
-	async run(interaction) { }
+	/**
+	 * @param {import("discord.js").ChatInputCommandInteraction} interaction
+	 */
+	async run(interaction) {
+		/** @type {import("client")} */
+		const client = this.client;
+
+		await interaction.deferReply({ ephemeral: true });
+
+		const ticket = await client.prisma.ticket.findUnique({
+			include: { guild: true },
+			where: { id: interaction.options.getString('ticket', false) || interaction.channel.id },
+		});
+
+		if (!ticket) {
+			const settings = await client.prisma.guild.findUnique({ where: { id: interaction.guild.id } });
+			const getMessage = client.i18n.getLocale(settings.locale);
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: settings.footer,
+					})
+						.setColor(settings.errorColour)
+						.setTitle(getMessage('misc.invalid_ticket.title'))
+						.setDescription(getMessage('misc.invalid_ticket.description')),
+				],
+			});
+		}
+
+		const getMessage = client.i18n.getLocale(ticket.guild.locale);
+
+		if (
+			ticket.id !== interaction.channel.id &&
+			ticket.createdById !== interaction.member.id &&
+			!(await isStaff(interaction.guild, interaction.member.id))
+		) {
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder({
+						iconURL: interaction.guild.iconURL(),
+						text: ticket.guild.footer,
+					})
+						.setColor(ticket.guild.errorColour)
+						.setTitle(getMessage('commands.slash.remove.not_staff.title'))
+						.setDescription(getMessage('commands.slash.remove.not_staff.description')),
+				],
+			});
+		}
+
+		/** @type {import("discord.js").TextChannel} */
+		const ticketChannel = await interaction.guild.channels.fetch(ticket.id);
+		const member = interaction.options.getMember('member', true);
+
+		if (member.id === client.user.id) {
+			return await interaction.editReply({
+				embeds: [
+					new ExtendedEmbedBuilder()
+						.setColor(ticket.guild.errorColour)
+						.setTitle('❌'),
+				],
+			});
+		}
+
+		await ticketChannel.permissionOverwrites.delete(member, `${interaction.user.tag} removed ${member.user.tag} from the ticket`);
+
+		await ticketChannel.send({
+			embeds: [
+				new ExtendedEmbedBuilder()
+					.setColor(ticket.guild.primaryColour)
+					.setDescription(getMessage('commands.slash.remove.removed', {
+						by: interaction.member.toString(),
+						removed: member.toString(),
+					})),
+			],
+		});
+
+		await interaction.editReply({
+			embeds: [
+				new ExtendedEmbedBuilder({
+					iconURL: interaction.guild.iconURL(),
+					text: ticket.guild.footer,
+				})
+					.setColor(ticket.guild.successColour)
+					.setTitle(getMessage('commands.slash.remove.success.title'))
+					.setDescription(getMessage('commands.slash.remove.success.description', {
+						member: member.toString(),
+						ticket: ticketChannel.toString(),
+					})),
+			],
+		});
+
+		logTicketEvent(this.client, {
+			action: 'update',
+			diff: {
+				original: { [getMessage('log.ticket.removed')]: member.user.tag },
+				updated: {},
+			},
+			target: {
+				id: ticket.id,
+				name: `<#${ticket.id}>`,
+			},
+			userId: interaction.user.id,
+		});
+	}
 };
