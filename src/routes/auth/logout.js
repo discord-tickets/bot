f55ee02ce51e84f45d6100194665a360a3038349
Isaac <git@eartharoid.me>
2023-03-10 23:42:39 +0000

    fix: revoke token on logout

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,7 +1,13 @@
-module.exports.get = () => ({
+module.exports.get = fastify => ({
 	handler: async function (req, res) {
+		await fetch('https://discord.com/api/oauth2/token/revoke', {
+			body: new URLSearchParams({ token: req.user.payload.accessToken }).toString(),
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			method: 'POST',
+		});
 		res
 			.clearCookie('token', '/')
-			.send('Logged out.');
+			.send('The token has been revoked.');
 	},
+	onRequest: [fastify.authenticate],
 });
