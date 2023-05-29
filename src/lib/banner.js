6f227f2f48c17d375f58f2c60ff20355e940918a
Isaac <git@eartharoid.me>
2023-05-29 18:09:04 +0100

    style: change banner text

--- src/lib/banner.js
+++ src/lib/banner.js
@@ -5,5 +5,5 @@ const link = require('terminal-link');
 module.exports = version => colours.cyan(figlet.textSync('Discord', { font: 'Banner3' })) +
 	colours.cyan('\n\n' + figlet.textSync('Tickets', { font: 'Banner3' })) +
 	colours.cyanBright(`\n\n${link('Discord Tickets', 'https://discordtickets.app')} bot v${version} by eartharoid`) +
-	colours.cyanBright('\n' + link('Sponsor this project', 'https://discordtickets.app/sponsor')) +
+	colours.cyanBright('\nSponsor this project at https://discordtickets.app/sponsor') +
 	'\n\n';
