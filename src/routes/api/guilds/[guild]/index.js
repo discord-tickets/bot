b00d2f312eb0e3353213a1ebdcf28002342640ec
Isaac <git@eartharoid.me>
2024-07-03 02:03:16 +0100

    feat(api): something

--- src/routes/api/guilds/[guild]/index.js
+++ src/routes/api/guilds/[guild]/index.js
@@ -5,7 +5,6 @@ module.exports.get = fastify => ({
 	handler: async (req, res) => {
 		const { client } = req.routeOptions.config;
 		const guild = client.guilds.cache.get(req.params.guild);
-		if (!guild) return res.status(404).send(new Error('Not Found'));
 		res.send({
 			id: guild.id,
 			logo: iconURL(guild),
