9689648a6c4dd1e05ed664f7be499f3c0d834124
Isaac <git@eartharoid.me>
2025-02-01 03:14:32 +0000

    fix(api): missing archived messages etc in exports

--- src/lib/workers/export.js
+++ src/lib/workers/export.js
@@ -13,18 +13,18 @@ expose({
 		if (ticket.feedback?.comment) ticket.feedback.comment = decrypt(ticket.feedback.comment);
 		if (ticket.topic) ticket.topic = decrypt(ticket.topic);
 
-		ticket.archivedMessages = ticket.archivedMessages.map(async message => {
+		ticket.archivedMessages = ticket.archivedMessages.map(message => {
 			message.content = decryptIfExists(message.content);
 			return message;
 		});
 
-		ticket.archivedUsers = ticket.archivedUsers.map(async user => {
+		ticket.archivedUsers = ticket.archivedUsers.map(user => {
 			user.displayName = decryptIfExists(user.displayName);
 			user.username = decryptIfExists(user.username);
 			return user;
 		});
 
-		ticket.questionAnswers = ticket.questionAnswers.map(async answer => {
+		ticket.questionAnswers = ticket.questionAnswers.map(answer => {
 			if (answer.value) answer.value = decryptIfExists(answer.value);
 			return answer;
 		});
