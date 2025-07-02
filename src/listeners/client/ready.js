a5d409860c27fd0da7305fe8a4672d62cf89725b
Isaac <git@eartharoid.me>
2025-07-02 17:25:45 +0100

    fix: disable stale ticket handling on public bots
    because it still causes a memory leak and the previous attempt to fix introduced a new (spammy) bug

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -2,17 +2,11 @@ const { Listener } = require('@eartharoid/dbf');
 const ms = require('ms');
 const sync = require('../../lib/sync');
 const checkForUpdates = require('../../lib/updates');
-const { isStaff } = require('../../lib/users');
-const {
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-} = require('discord.js');
-const ExtendedEmbedBuilder = require('../../lib/embed');
 const {
 	getAverageTimes,
 	sendToHouston,
 } = require('../../lib/stats');
+const handleStaleTickets = require('../../lib/stale');
 
 module.exports = class extends Listener {
 	constructor(client, options) {
@@ -115,118 +109,13 @@ module.exports = class extends Listener {
 			setInterval(() => checkForUpdates(client), ms('1w'));
 		}
 
-		// send inactivity warnings and close stale tickets
-		const staleInterval = ms('15m');
-		setInterval(async () => {
-			client.log.info.cron('Handling stale tickets');
-			const closeCommand = client.application.commands.cache.find(c => c.name === 'close');
-			const guilds = await client.prisma.guild.findMany({
-				include: {
-					tickets: {
-						include: { category: true },
-						where: { open: true },
-					},
-				},
-				// where: { staleAfter: { not: null } },
-				where: { staleAfter: { gte: staleInterval } },
-			});
-			let processed = 0;
-			let closed = 0;
-			let marked = 0;
-
-			for (const guild of guilds) {
-				const getMessage = client.i18n.getLocale(guild.locale);
-				for (const ticket of guild.tickets) {
-					try {
-						processed++;
-						if (client.tickets.$stale.has(ticket.id)) {
-							const $ = client.tickets.$stale.get(ticket.id);
-							const autoCloseAfter = $.closeAt - $.staleSince;
-							const halfway = $.closeAt - (autoCloseAfter / 2);
-							const channel = client.channels.cache.get(ticket.id);
-							if (!channel) {
-								client.tickets.$stale.delete(ticket.id);
-								continue;
-							}
-							if (Date.now() >= halfway && Date.now() < halfway + staleInterval) {
-								await channel.send({
-									embeds: [
-										new ExtendedEmbedBuilder()
-											.setColor(guild.primaryColour)
-											.setTitle(getMessage('ticket.closing_soon.title'))
-											.setDescription(getMessage('ticket.closing_soon.description', { timestamp: Math.floor(($.closeAt + staleInterval) / 1000) })),
-									],
-								});
-							} else if ($.closeAt < Date.now()) {
-								await client.tickets.finallyClose(ticket.id, $);
-								closed++;
-							}
-						} else if (Date.now() - (ticket.lastMessageAt || ticket.createdAt) >= guild.staleAfter) {
-						// set as stale
-						/** @type {import("discord.js").TextChannel} */
-							const channel = await client.channels.fetch(ticket.id);
-							if (!channel) {
-								await client.tickets.finallyClose(ticket.id, { reason: 'channel deleted' });
-								closed++;
-								continue;
-							}
-							const messages = (await channel.messages.fetch({ limit: 5 })).filter(m => m.author.id !== client.user.id);
-							let ping = '';
-
-							if (messages.size > 0) {
-								const lastMessage =  messages.first();
-								const staff = await isStaff(channel.guild, lastMessage.author.id);
-								if (staff) ping = `<@${ticket.createdById}>`;
-								else ping = ticket.category.pingRoles.map(r => `<@&${r}>`).join(' ');
-							}
-
-							const sent = await channel.send({
-								components: [
-									new ActionRowBuilder()
-										.addComponents(
-											new ButtonBuilder()
-												.setCustomId(JSON.stringify({ action: 'close' }))
-												.setStyle(ButtonStyle.Danger)
-												.setEmoji(getMessage('buttons.close.emoji'))
-												.setLabel(getMessage('buttons.close.text')),
-										),
-								],
-								content: ping,
-								embeds: [
-									new ExtendedEmbedBuilder({
-										iconURL: channel.guild.iconURL(),
-										text: guild.footer,
-									})
-										.setColor(guild.primaryColour)
-										.setTitle(getMessage('ticket.inactive.title'))
-										.setDescription(getMessage('ticket.inactive.description', {
-											close: `</${closeCommand.name}:${closeCommand.id}>`,
-											timestamp: Math.floor((ticket.lastMessageAt || ticket.createdAt).getTime() / 1000),
-										})),
-								],
-							});
-
-							client.tickets.$stale.set(ticket.id, {
-								closeAt: guild.autoClose ? Date.now() + guild.autoClose : null,
-								closedBy: null,
-								message: sent,
-								messages: 0,
-								reason: 'inactivity',
-								staleSince: Date.now(),
-							});
-							marked++;
-						}
-					} catch (error) {
-						client.log.error(error);
-					}
-				}
-			}
-			client.log.success.cron({
-				closed,
-				marked,
-				processed,
-				stale: client.tickets.$stale.size,
-			});
-		}, staleInterval);
+		if (process.env.PUBLIC_BOT === 'true') {
+			client.log.notice('Inactivity warnings and auto-close features are disabled');
+			client.log.warn('Unset PUBLIC_BOT to re-enable stale ticket handling');
+		} else {
+			// send inactivity warnings and close stale tickets
+			const staleInterval = ms('15s');
+			setInterval(() => handleStaleTickets(client, staleInterval), staleInterval);
+		}
 	}
 };
