c64e3632dbdde726b1df96e6266e924097846844
Flexusma <info@flexusma.de>
2025-07-14 17:37:59 +0200

    Improve thread amount/parallelisation and overall export handling

--- src/lib/workers/export.js
+++ src/lib/workers/export.js
@@ -2,35 +2,53 @@ const { expose } = require('threads/worker');
 const { decrypt } = require('../crypto');
 
 expose({
-	exportTicket(ticket) {
-		ticket.archivedMessages = ticket.archivedMessages.map(message => {
-			message.content &&= decrypt(message.content);
-			return message;
-		});
+	/**
+	 * Exports a batch of tickets in a single worker thread
+	 * @param batch
+	 */
+	exportTicketBatch(batch){
+		function exportTicket(ticket) {
+			ticket.archivedMessages = ticket.archivedMessages.map(message => {
+				try {
+					message.content &&= decrypt(message.content);
+					return message;
+				}catch (e) {
+					return undefined;
+				}
+			});
 
-		ticket.archivedUsers = ticket.archivedUsers.map(user => {
-			user.displayName &&= decrypt(user.displayName);
-			user.username &&= decrypt(user.username);
-			return user;
-		});
+			ticket.archivedUsers = ticket.archivedUsers.map(user => {
+				user.displayName &&= decrypt(user.displayName);
+				user.username &&= decrypt(user.username);
+				return user;
+			});
 
-		if (ticket.feedback) {
-			// why is feedback the only one with a guild relation? 😕
-			delete ticket.feedback.guildId;
-			ticket.feedback.comment &&= decrypt(ticket.feedback.comment);
+			if (ticket.feedback) {
+				// why is feedback the only one with a guild relation? 😕
+				delete ticket.feedback.guildId;
+				ticket.feedback.comment &&= decrypt(ticket.feedback.comment);
+			}
+
+			ticket.closedReason &&= decrypt(ticket.closedReason);
+
+			delete ticket.guildId;
+
+			ticket.questionAnswers = ticket.questionAnswers.map(answer => {
+				answer.value &&= decrypt(answer.value);
+				return answer;
+			});
+
+			ticket.topic &&= decrypt(ticket.topic);
+
+			return JSON.stringify(ticket);
 		}
 
-		ticket.closedReason &&= decrypt(ticket.closedReason);
-
-		delete ticket.guildId;
-
-		ticket.questionAnswers = ticket.questionAnswers.map(answer => {
-			answer.value &&= decrypt(answer.value);
-			return answer;
-		});
-
-		ticket.topic &&= decrypt(ticket.topic);
-
-		return JSON.stringify(ticket);
+		return batch.map(ticket => {
+			try {
+				return exportTicket(ticket);
+			}catch (err){ /* empty */ }
+		})
+			.filter(Boolean)
+			.join('\n')+ '\n';
 	},
 });
