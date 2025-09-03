8cd665c1564b6f28bb93094b9b591543e72e38a0
Enno Gelhaus <egelhaus@ennogelhaus.de>
2025-09-03 17:37:28 +0200

    Add logging and profiling options to Sentry init

--- src/sentry-init.js
+++ src/sentry-init.js
@@ -4,14 +4,19 @@ const Sentry = require('@sentry/node');
 
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
-	// Profiling
 	integrations: [
+	    // Profiling
 		nodeProfilingIntegration(),
+        // Send console logs to Sentry
+		Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
 	],
 	// Profiling
 	profileLifecycle: 'trace',
 	profileSessionSampleRate: parseFloat(process.env.SENTRY_PROFILING_RATE?? 1.0),
 
+	// Logging
+	enableLogs: process.env.SENTRY_LOGGING === 'true'´,
+	
 	// Setting this option to true will send default PII data to Sentry.
 	// For example, automatic IP address collection on events
 	sendDefaultPii: true,
